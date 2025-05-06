995ba4d23ab152fe402e1af050c16bb22fc57fc4
Emilianomarcab <emilianomarcab@gmail.com>
2025-05-06 16:04:20 -0600

    Acutlizacion comeeeeeew

--- carrusel.js
+++ carrusel.js
@@ -1,14 +1,25 @@
-
 const imagenes = [
-    { src: 'img/Cafe4.jpg', texto: 'Descripción de la imagen 1' },
-    { src: 'img/Cafe2.jpg', texto: 'Descripción de la imagen 2' },
-    { src: 'img/Cafe3.jpg', texto: 'Descripción de la imagen 3' }
+    'img/Cafe4.jpg',
+    'img/Cafe2.jpg',
+    'img/Cafe3.jpg'
 ];
+
+// Pre-cargar las imágenes
+imagenes.forEach(function(imagen) {
+    const img = new Image();
+    img.src = imagen;
+});
 
 let indiceActual = 0;
 
 function cambiarImagen(direccion) {
     indiceActual = (indiceActual + direccion + imagenes.length) % imagenes.length;
-    document.getElementById('imagen-carrusel').src = imagenes[indiceActual].src;
-    document.getElementById('texto-imagen').textContent = imagenes[indiceActual].texto;
+    actualizarImagen();
 }
+
+function actualizarImagen() {
+    document.getElementById('imagen-carrusel').src = imagenes[indiceActual];
+    document.getElementById('texto-imagen').textContent = 'Descripción de la imagen ' + (indiceActual + 1);
+}
+
+console.log("Sitio Real Rentería cargado correctamente.");
