bcb389947e80503263a48694a7999fda4a7e8c7c
Emilianomarcab <emilianomarcab@gmail.com>
2025-05-27 13:05:45 -0600

    Actualización de la página

--- carrusel.js
+++ carrusel.js
@@ -1,25 +1,32 @@
 const imagenes = [
-    'img/Cafe4.jpg',
-    'img/Cafe2.jpg',
-    'img/Cafe3.jpg'
-];
-
-// Pre-cargar las imágenes
-imagenes.forEach(function(imagen) {
+    'img/Experiencia1.jpg',
+    'img/Experiencia2.jpg',
+    'img/Experiencia3.jpg'
+  ];
+  
+  const descripciones = [
+    'Ofrecemos un lugar tranquilo con jazz de fondo para tener una platica amena mientras si disfurta un Cafe',
+    'Ideal para venir a leer o trabajar de manera tranquila con internet ',
+    'Ambiente interior de Real Rentería'
+  ];
+  
+  // Pre-cargar las imágenes
+  imagenes.forEach(function(imagen) {
     const img = new Image();
     img.src = imagen;
-});
-
-let indiceActual = 0;
-
-function cambiarImagen(direccion) {
+  });
+  
+  let indiceActual = 0;
+  
+  function cambiarImagen(direccion) {
     indiceActual = (indiceActual + direccion + imagenes.length) % imagenes.length;
     actualizarImagen();
-}
-
-function actualizarImagen() {
+  }
+  
+  function actualizarImagen() {
     document.getElementById('imagen-carrusel').src = imagenes[indiceActual];
-    document.getElementById('texto-imagen').textContent = 'Descripción de la imagen ' + (indiceActual + 1);
-}
-
-console.log("Sitio Real Rentería cargado correctamente.");
+    document.getElementById('texto-imagen').textContent = descripciones[indiceActual];
+  }
+  
+  console.log("Sitio Real Rentería cargado correctamente.");
+  
